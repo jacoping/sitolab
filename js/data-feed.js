263060842dd808d6251877906bf828e5e8ac186c
jacoping <jacoping@gmail.com>
2020-10-06 18:08:49 +0200

    sistemazione feed - step 1

---
+++ js/data-feed.js
@@ -0,0 +1,101 @@
+
+
+
+
+
+
+var data_feed = {
+  project_updates: [{
+      nome_progetto: "Wayfinding Università di Firenze",
+      data_post: "21/07/2019",
+      titolo: "Inizio lavori San Marco",
+      image: 'https://picsum.photos/200/300',
+      testo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tempor fringilla velit, eu blandit justo semper eget. Nulla arcu augue, vehicula nec mi sed, aliquet facilisis velit."
+    },
+    {
+      nome_progetto: "Sistema Museale di Ateneo Firenze",
+      data_post: "21/07/2019",
+      titolo: "Nuova identità del Sistema Museale di Ateneo",
+      image: 'https://picsum.photos/200/150',
+      testo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tempor fringilla velit, eu blandit justo semper eget. Nulla arcu augue, vehicula nec mi sed, aliquet facilisis velit."
+    },
+    {
+      titolo: "Stradesign",
+      image: 'https://picsum.photos/200/250'
+    },
+    {
+      titolo: "Inizio montaggio Palazzo Fenzi",
+      image: 'https://picsum.photos/200/157'
+    },
+    {
+      titolo: "Call Immatricolazioni",
+      image: 'https://picsum.photos/200/257'
+    }
+  ],
+  corsi_workshop: [{
+      data_post: "01/09/2020",
+      data_corso: "18-20 settembre 2020",
+      titolo: "Social Design for Sustainable Cities",
+      sottotitolo: "Workshop organized by European National Institutes for Culture & Polish-Japanese Academy of Information Technologies",
+      image: 'https://picsum.photos/200/120',
+    },
+    {
+      titolo: "CDM2020",
+      data_post: "30/07/2020",
+      data_corso: "aprile-maggio 2020",
+      sottotitolo: "Corso di Laurea Magistrale Design",
+      image: 'https://picsum.photos/200/200',
+      testo: "Il periodo di blocco indotto dalla pandemia di Covid-19 è stato alla base di questo progetto realizzato con gli studenti al primo anno della Magistrale in Design"
+    },
+    {
+      titolo: "Stradesign",
+      image: 'https://picsum.photos/200/135'
+    },
+    {
+      titolo: "Inizio montaggio Palazzo Fenzi",
+      image: 'https://picsum.photos/200/180'
+    },
+    {
+      titolo: "Call Immatricolazioni",
+      image: 'https://picsum.photos/200/302'
+    }
+  ],
+  pubblicazioni: [{
+      titolo: "Inizio lavori San Marco",
+      image: 'https://picsum.photos/200/140'
+    },
+    {
+      titolo: "Nuova identità del Sistema Museale di Ateneo",
+      image: 'https://picsum.photos/200/210'
+    },
+    {
+      titolo: "Stradesign",
+      image: 'https://picsum.photos/200/130'
+    },
+    {
+      titolo: "Inizio montaggio Palazzo Fenzi",
+      image: 'https://picsum.photos/200/206'
+    }
+  ],
+  exhibition_talks: [{
+      titolo: "Inizio lavori San Marco",
+      image: 'https://picsum.photos/200/145'
+    },
+    {
+      titolo: "Nuova identità del Sistema Museale di Ateneo",
+      image: 'https://picsum.photos/200/222'
+    },
+    {
+      titolo: "Stradesign",
+      image: 'https://picsum.photos/200/312'
+    },
+    {
+      titolo: "Inizio montaggio Palazzo Fenzi",
+      image: 'https://picsum.photos/200/130'
+    },
+    {
+      titolo: "Call Immatricolazioni",
+      image: 'https://picsum.photos/200/65'
+    }
+  ]
+}
