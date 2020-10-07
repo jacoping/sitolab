98aae7cbdc06a9b46f60fa5ee350b5ce7439d837
Jacopo <jacoping@gmail.com>
2020-10-07 13:08:05 +0200

    sisetmazione feed - step 2

--- js/data-feed.js
+++ js/data-feed.js
@@ -1,101 +1,72 @@
-
-
-
-
-
+/*
+---
+---
+DATI GREZZI PER LE PROVE
+---
+---
+*/
 
 var data_feed = {
   project_updates: [{
       nome_progetto: "Wayfinding Università di Firenze",
       data_post: "21/07/2019",
       titolo: "Inizio lavori San Marco",
-      image: 'https://picsum.photos/200/300',
-      testo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tempor fringilla velit, eu blandit justo semper eget. Nulla arcu augue, vehicula nec mi sed, aliquet facilisis velit."
+      image: 'https://admin.lab.didacommunicationlab.it/uploads/lab/originals/113e8bc6-7fd8-439c-b5fa-2ed1f985a2b3.jpg',
+      testo: "Iniziano oggi le installazioni del progetto Wayfinding di Ateneo nel palazzo del Rettorato in piazza San Marco a Firenze"
     },
     {
       nome_progetto: "Sistema Museale di Ateneo Firenze",
       data_post: "21/07/2019",
       titolo: "Nuova identità del Sistema Museale di Ateneo",
-      image: 'https://picsum.photos/200/150',
-      testo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tempor fringilla velit, eu blandit justo semper eget. Nulla arcu augue, vehicula nec mi sed, aliquet facilisis velit."
-    },
-    {
-      titolo: "Stradesign",
-      image: 'https://picsum.photos/200/250'
-    },
-    {
-      titolo: "Inizio montaggio Palazzo Fenzi",
-      image: 'https://picsum.photos/200/157'
-    },
-    {
-      titolo: "Call Immatricolazioni",
-      image: 'https://picsum.photos/200/257'
+      image: 'https://admin.lab.didacommunicationlab.it/uploads/lab/originals/5bc8fe05-1cba-461a-a3a6-bc348f91358d.jpg',
+      testo: "Conferenza Stampa per la presentazione all’interno del Museo Antropologico di Palazzo Non Finito"
     }
   ],
+
+
   corsi_workshop: [{
       data_post: "01/09/2020",
       data_corso: "18-20 settembre 2020",
       titolo: "Social Design for Sustainable Cities",
       sottotitolo: "Workshop organized by European National Institutes for Culture & Polish-Japanese Academy of Information Technologies",
-      image: 'https://picsum.photos/200/120',
+      image: 'https://admin.lab.didacommunicationlab.it/uploads/lab/originals/27adc5d1-a3cd-4732-be52-8828a452c557.png',
     },
     {
       titolo: "CDM2020",
       data_post: "30/07/2020",
       data_corso: "aprile-maggio 2020",
       sottotitolo: "Corso di Laurea Magistrale Design",
-      image: 'https://picsum.photos/200/200',
+      image: 'https://admin.lab.didacommunicationlab.it/uploads/lab/originals/42169d96-9575-4386-a160-bf3c2730d72f.png',
       testo: "Il periodo di blocco indotto dalla pandemia di Covid-19 è stato alla base di questo progetto realizzato con gli studenti al primo anno della Magistrale in Design"
-    },
-    {
-      titolo: "Stradesign",
-      image: 'https://picsum.photos/200/135'
-    },
-    {
-      titolo: "Inizio montaggio Palazzo Fenzi",
-      image: 'https://picsum.photos/200/180'
-    },
-    {
-      titolo: "Call Immatricolazioni",
-      image: 'https://picsum.photos/200/302'
     }
   ],
+
+
   pubblicazioni: [{
-      titolo: "Inizio lavori San Marco",
-      image: 'https://picsum.photos/200/140'
+      titolo: "Contenuto e forma",
+      sottotitolo: "Lo sviluppo della comunicazione visiva nella relazione tra ricerca e pratica progettuale",
+      autori: "<p>a cura di <b>Susanna Cerri</b></p><p> con i contributi di <b>Jacopo Ammendola, Gianluca Buoncore, Siliva Cattiodoro, Giacomo Dallatorre, Gaia Lavoratti, Vittoria Niccolini, Maddalena Rossi, Alice Trematerra</b></p>",
+      image: 'https://admin.lab.didacommunicationlab.it/uploads/lab/originals/ab714358-b25c-4d0b-9337-9ab6aaf76bc2.png'
     },
     {
-      titolo: "Nuova identità del Sistema Museale di Ateneo",
-      image: 'https://picsum.photos/200/210'
-    },
-    {
-      titolo: "Stradesign",
-      image: 'https://picsum.photos/200/130'
-    },
-    {
-      titolo: "Inizio montaggio Palazzo Fenzi",
-      image: 'https://picsum.photos/200/206'
+      titolo: "Identità migranti",
+      autori: "<b>Susanna Cerri, Maddalena Rossi</b>",
+      image: 'https://admin.lab.didacommunicationlab.it/uploads/lab/originals/98b27317-32aa-4980-ae85-ac3ca87a941e.png',
+      descrizione: "Segni per una nuova geografia dell’accoglienza presentato dal Laboratorio di Comunicazione della Scuola di Architettura dell’Università di Firenze alla conferenza Inscription en relation. Des traces coloniales aux expressions plurielles, tenutasi a Parigi nel febbraio 2020. Il progetto si concentra sulla lettura e sull’analisi critica del sistema di segni prodotti dai complessi meccanismi e dispositivi spaziali attraverso i quali in Italia si articola l’accoglienza dei flussi migratori provenienti dal Mediterraneo, studiando la composita mappa di luoghi, strutture e barriere fisiche e linguistiche che il corpo migrante attraversa nel suo viaggio verso e dentro la penisola."
     }
   ],
+
+
   exhibition_talks: [{
-      titolo: "Inizio lavori San Marco",
-      image: 'https://picsum.photos/200/145'
+      data_post: "14-16/02/2020",
+      titolo: "Inscription en relation",
+      image: 'https://admin.lab.didacommunicationlab.it/uploads/lab/originals/2f6bf5be-f334-4b2b-9999-8f7b8a0ad82f.png',
+      descrizione: "didacommunicationlab è a Parigi con Ruedi e Vera Baur. <em>Inscription en relation:</em> un progetto internazionale e multidisciplinare con lo scopo di dare voce alle lingue assenti, e mettere in discussione l'iscrizione delle lingue nello spazio pubblico. 21 Università di tutto il mondo al Palais de la Porte Dorée di Parigi dal 14 al 16 febbraio 2020."
     },
     {
-      titolo: "Nuova identità del Sistema Museale di Ateneo",
-      image: 'https://picsum.photos/200/222'
-    },
-    {
-      titolo: "Stradesign",
-      image: 'https://picsum.photos/200/312'
-    },
-    {
-      titolo: "Inizio montaggio Palazzo Fenzi",
-      image: 'https://picsum.photos/200/130'
-    },
-    {
-      titolo: "Call Immatricolazioni",
-      image: 'https://picsum.photos/200/65'
+      dada_post: "02/04/2020",
+      titolo: "Incontro con Ruedi Baur",
+      image: 'https://admin.lab.didacommunicationlab.it/uploads/lab/originals/67ac3ec7-eb13-45d7-af7a-0f00383cc651.jpg'
     }
   ]
 }
