38c44ce9b9cbfe972a390c8062bf6c3780f7e39f
jacoping <jacoping@gmail.com>
2020-10-12 16:44:32 +0200

    update feed

--- js/data-feed.js
+++ js/data-feed.js
@@ -55,7 +55,7 @@ var data_feed = {
     {
       titolo: "Identità migranti",
       autori: "<b>Susanna Cerri, Maddalena Rossi</b>",
-      image: 'https://admin.lab.didacommunicationlab.it/uploads/lab/originals/98b27317-32aa-4980-ae85-ac3ca87a941e.png',
+      image: 'https://admin.lab.didacommunicationlab.it/uploads/lab/originals/identita-migranti.jpg',
       descrizione: "Segni per una nuova geografia dell’accoglienza presentato dal Laboratorio di Comunicazione della Scuola di Architettura dell’Università di Firenze alla conferenza Inscription en relation. Des traces coloniales aux expressions plurielles, tenutasi a Parigi nel febbraio 2020. Il progetto si concentra sulla lettura e sull’analisi critica del sistema di segni prodotti dai complessi meccanismi e dispositivi spaziali attraverso i quali in Italia si articola l’accoglienza dei flussi migratori provenienti dal Mediterraneo, studiando la composita mappa di luoghi, strutture e barriere fisiche e linguistiche che il corpo migrante attraversa nel suo viaggio verso e dentro la penisola."
     }
   ],
@@ -64,11 +64,12 @@ var data_feed = {
   exhibition_talks: [
     {
       data_post: "01/02/2020",
-      data_corso: "14-16 febbraio 2020",
-      titolo: "Inscription en relation",
-      sottotitolo: "Convegno internazionale",
+      data_corso: "10-11 ottobre 2020",
+      titolo: "Strange night of design",
+      sottotitolo: "24h di dialoghi",
       image: 'https://admin.lab.didacommunicationlab.it/uploads/lab/originals/rb.gif',
-      descrizione: "didacommunicationlab è a Parigi con Ruedi e Vera Baur. <em>Inscription en relation</em>: un progetto internazionale e multidisciplinare con lo scopo di dare voce alle lingue assenti, e mettere in discussione l'iscrizione delle lingue nello spazio pubblico. 21 Università di tutto il mondo al Palais de la Porte Dorée di Parigi dal 14 al 16 febbraio 2020."
+      descrizione: "A program proposed by Civic City as part of <br><b>Lille Metropole 2020 World Design Capital</b><br> in partnership with the French institute and the World Design Organization.",
+      descrizione2: "24 hours of dialogues on the present and future of design, a night of reflection with more than 70 speakers from all over the world. This is what Ruedi and Vera Baur from Civic City association are proposing between Saturday 10th and Sunday 11th October at the Tri Postal in Lille and broadcast on social media. In order to collectively think about the role of design in the future of our society, the program is built around a series of interviews with creators, personalities and design theorists developing a critical thinking on their activity.",
     },
     {
       data_post: "01/02/2020",
@@ -76,7 +77,8 @@ var data_feed = {
       titolo: "Inscription en relation",
       sottotitolo: "Convegno internazionale",
       image: 'https://admin.lab.didacommunicationlab.it/uploads/lab/originals/2f6bf5be-f334-4b2b-9999-8f7b8a0ad82f.png',
-      descrizione: "didacommunicationlab è a Parigi con Ruedi e Vera Baur. <em>Inscription en relation</em>: un progetto internazionale e multidisciplinare con lo scopo di dare voce alle lingue assenti, e mettere in discussione l'iscrizione delle lingue nello spazio pubblico. 21 Università di tutto il mondo al Palais de la Porte Dorée di Parigi dal 14 al 16 febbraio 2020."
+      descrizione: "didacommunicationlab è a Parigi con Ruedi e Vera Baur. <em>Inscription en relation</em>: un progetto internazionale e multidisciplinare con lo scopo di dare voce alle lingue assenti, e mettere in discussione l'iscrizione delle lingue nello spazio pubblico. 21 Università di tutto il mondo al Palais de la Porte Dorée di Parigi dal 14 al 16 febbraio 2020.",
+      descrizione2: "didacommunicationlab è a Parigi con Ruedi e Vera Baur. <em>Inscription en relation</em>: un progetto internazionale e multidisciplinare con lo scopo di dare voce alle lingue assenti, e mettere in discussione l'iscrizione delle lingue nello spazio pubblico. 21 Università di tutto il mondo al Palais de la Porte Dorée di Parigi dal 14 al 16 febbraio 2020."
     },
     {
       dada_post: "02/04/2020",
